1856d4c9dda0f44fd38124d17bbf7b1578540afc
Andrew Pye <professor_smart@hotmail.com>
2023-09-03 10:35:13 +1000

    Yeetus Deletus state machine

--- Actions.cs
+++ Actions.cs
@@ -1,94 +1,78 @@
-using System.Reactive.Linq;
 using RoboMaster;
-using Stateless;
 
 public class Actions
 {
     private RoboMasterClient robot;
-    private StateMachine<RobotState, RobotTrigger> robotState;
 
-    private bool active = true;
-
-    public Actions(RoboMasterClient robot, StateMachine<RobotState, RobotTrigger> robotState)
+    public Actions(RoboMasterClient robot)
     {
         this.robot = robot;
-        this.robotState = robotState;
-
-        var weakRefThis = new WeakReference<Actions>(this);
-        robotState.OnTransitioned(_ =>
-        {
-            if (weakRefThis.TryGetTarget(out var self))
-            {
-                self.active = false;
-            }
-        });
     }
 
-    private async Task<bool> SafeFireAsync(RobotTrigger trigger)
+    private static Task<StopReason> RunLoop(Func<Task<StopReason?>> action, CancellationToken cancellationToken)
     {
-        if (active)
-        {
-            active = false;
-            return await robotState.SafeFireAsync(trigger);
-        }
-        else return false;
-    }
+        var taskCompletionSource = new TaskCompletionSource<StopReason>();
 
-    private void RunLoop(Func<Task<RobotTrigger?>> action)
-    {
         new Thread(async () =>
         {
-            while (active)
+            while (!cancellationToken.IsCancellationRequested)
             {
-                var trigger = await action();
+                var result = await action();
 
-                if (trigger != null)
+                if (result != null)
                 {
-                    await SafeFireAsync(trigger.Value);
+                    taskCompletionSource.SetResult(result.Value);
                     break;
                 }
+            }
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                taskCompletionSource.SetCanceled();
             }
         })
         {
             IsBackground = true
         }.Start();
+
+        return taskCompletionSource.Task;
     }
 
-    public void LookForObstacles(float minDistance = 30)
+    public Task<StopReason> LookForObstacles(float minDistance = 30, CancellationToken? cancellationToken = null)
     {
-        RunLoop(async () =>
+        return RunLoop(async () =>
         {
             var distance = await robot.GetIRDistance(1);
 
             if (distance < minDistance)
             {
-                return RobotTrigger.ObstacleTooClose;
+                return StopReason.TooClose;
             }
 
             await Task.Delay(100);
 
             return null;
-        });
+        }, cancellationToken ?? CancellationToken.None);
     }
 
-    public void LookForNoObstacles(float minDistance = 30)
+    public Task<StopReason> LookForNoObstacles(float minDistance = 30, CancellationToken? cancellationToken = null)
     {
-        RunLoop(async () =>
+        return RunLoop(async () =>
         {
             var distance = await robot.GetIRDistance(1);
 
             if (distance >= minDistance)
             {
-                return RobotTrigger.ObstacleTooClose;
+                return StopReason.NotTooClose;
             }
 
             await Task.Delay(100);
 
             return null;
-        });
+        }, cancellationToken ?? CancellationToken.None);
     }
 
-    public async Task FollowLine(LineColour lineColour, float? speed = null)
+    public async Task<StopReason> FollowLine(LineColour lineColour, float? speed = null, CancellationToken? cancellationToken = null)
     {
         await robot.SetLineRecognitionColour(lineColour);
         
@@ -97,17 +81,11 @@ public class Actions
 
         var didSeeIntersection = false;
 
-        var lineEnumerator = robot.Line.ToDroppingAsyncEnumerable().GetAsyncEnumerator();
-        await lineEnumerator.MoveNextAsync();
-
-        RunLoop(async () =>
+        await foreach (var line in robot.Line.ToDroppingAsyncEnumerable())
         {
-            var line = lineEnumerator.Current;
+            if (cancellationToken?.IsCancellationRequested == true) return StopReason.Cancelled;
 
-            if (line.Type == LineType.None || line.Points.Length == 0)
-            {
-                return RobotTrigger.NoLineDetected;
-            }
+            if (line.Type == LineType.None || line.Points.Length == 0) return StopReason.NoLine;
 
             if (line.Type == LineType.Intersection)
             {
@@ -115,32 +93,116 @@ public class Actions
             }
             else if (didSeeIntersection)
             {
-                return RobotTrigger.IntersectionDetected;
+                return StopReason.Intersection;
             }
 
             var (leftSpeed, rightSpeed) = follower.GetWheelSpeed(line);
 
-            await Task.WhenAll(robot.SetWheelSpeed(rightSpeed, leftSpeed), lineEnumerator.MoveNextAsync().AsTask());
+            await robot.SetWheelSpeed(rightSpeed, leftSpeed);
+        }
 
-            return null;
-        });
+        throw new Exception("The line stream ended unexpectedly");
     }
 
-    public void LookForLine()
+    public async Task<StopReason> FindLine(LineColour lineColour, CancellationToken? cancellationToken = null)
     {
-        var lineEnumerator = robot.Line.ToDroppingAsyncEnumerable().GetAsyncEnumerator();
+        await robot.SetLineRecognitionColour(lineColour);
 
-        RunLoop(async () =>
+        var lineEnumerator = robot.Line.ToDroppingAsyncEnumerable().GetAsyncEnumerator();
+        await lineEnumerator.MoveNextAsync();
+
+        return await RunLoop(async () =>
         {
-            await lineEnumerator.MoveNextAsync();
             var line = lineEnumerator.Current;
 
-            if (line.Points.Length == 10 && line.Type == LineType.Straight)
+            if (line.Type != LineType.None && line.Points.Length != 0)
             {
-                return RobotTrigger.LineDetected;
+                return StopReason.Line;
             }
 
+            await lineEnumerator.MoveNextAsync();
+
             return null;
-        });
+        }, cancellationToken ?? CancellationToken.None);
     }
+
+    public async Task<StopReason> MoveToDepot()
+    {
+        #region Move Approximately to Line
+        await robot.Move(0, 0, -90);
+        await Task.Delay(3000);
+
+        await robot.SetWheelSpeed(30);
+        await Task.Delay(500);
+
+        await robot.SetWheelSpeed(-30, 30, 30, -30); // Move right
+        await Task.Delay(500);
+        #endregion
+
+        #region Center on Line
+        var targetX = 0.4f;
+        var margin = 0.05f;
+
+        await robot.SetLineRecognitionColour(LineColour.Red);
+
+        await foreach (var line in robot.Line.ToDroppingAsyncEnumerable())
+        {
+            var x = line.Points[0].X;
+
+            if (Math.Abs(x - targetX) < margin)
+            {
+                await robot.SetWheelSpeed(0);
+                break;
+            }
+
+            if (x < targetX) // Go Left
+            {
+                await robot.SetWheelSpeed(50, -50, -50, 50);
+            }
+            else // Go Right
+            {
+                await robot.SetWheelSpeed(-50, 50, 50, -50);
+            }
+        }
+        #endregion
+
+        #region Move to Depot
+        var cancellationTokenSource = new CancellationTokenSource();
+        
+        await await Task.WhenAny
+        (
+            FollowLine(LineColour.Red, speed: 40, cancellationToken: cancellationTokenSource.Token),
+            LookForObstacles(cancellationToken: cancellationTokenSource.Token)
+        );
+
+        cancellationTokenSource.Cancel();
+        #endregion
+
+        return StopReason.Completed;
+    }
+
+    public async Task<StopReason> ReturnFromDepot()
+    {
+        await robot.Move(0, 0, 180);
+        await Task.Delay(8000);
+
+        await robot.SetWheelSpeed(60);
+        await Task.Delay(500);
+
+        await robot.Move(0, 0, -90);
+        await Task.Delay(3000);
+
+        return StopReason.Completed;
+    }
+}
+
+public enum StopReason
+{
+    NoLine,
+    Line,
+    Intersection,
+    TooClose,
+    NotTooClose,
+    Completed,
+    Cancelled
 }
--- Program.cs
+++ Program.cs
@@ -2,9 +2,32 @@ using System.Reactive.Linq;
 using System.Diagnostics;
 
 using RoboMaster;
-using Stateless;
 
 using PathFinding;
+
+var robot = await RoboMasterClient.Connect(RoboMasterClient.DIRECT_CONNECT_IP);
+var actions = new Actions(robot);
+
+Console.Write("Enter direct control mode? (y/n, default n): ");
+
+if (Console.ReadKey().KeyChar == 'y')
+{
+    Console.WriteLine();
+    Console.WriteLine("Direct control mode enabled");
+
+    Console.Write("Enter command: ");
+    var command = (Console.ReadLine() ?? "").Split(' ');
+
+    while (command.Length != 0)
+    {
+        await robot.Do(command.Select(arg => new CommandArg(arg)).ToArray());
+
+        Console.Write("Enter command: ");
+        command = (Console.ReadLine() ?? "").Split(' ');
+    }
+
+    return;
+}
 
 var trackText = await File.ReadAllTextAsync("track.txt");
 var track = TrackParser.ParseTrack(trackText);
@@ -30,269 +53,151 @@ Debug.Assert(track.ContainsKey(drop), "Drop intersection not found");
 var route = RoutePlanner.FindRoute(track[start], track[pickup], track[drop]);
 Console.WriteLine($"Route: {string.Join(", ", route)}");
 
-var intersectionCount = 0;
-var currentConnection = route[0];
+var intersectionCount = -1;
 
-var robot = await RoboMasterClient.Connect(RoboMasterClient.DIRECT_CONNECT_IP);
+await robot.OpenGripper();
 
-#region State Machine
-var robotState = new StateMachine<RobotState, RobotTrigger>(RobotState.RedStopped);
+Console.WriteLine("Paused. Press any key to start...");
+await Task.Run(Console.ReadKey);
 
-robotState.OnTransitioned(transition =>
+await robot.SetLineRecognitionEnabled();
+await robot.SetIrEnabled();
+
+while (true)
 {
-    Console.WriteLine($"Transitioning to {transition.Destination} because ${transition.Trigger}");
-});
-
-robotState.Configure(RobotState.FollowingRedLine)
-    .OnEntryAsync(async () =>
+    #region Follow Red Line
     {
-        var actions = new Actions(robot, robotState);
+        var cancellationTokenSource = new CancellationTokenSource();
+        
+        var stoppedReason = await await Task.WhenAny
+        (
+            actions.FollowLine(LineColour.Red, cancellationToken: cancellationTokenSource.Token),
+            actions.LookForObstacles(cancellationToken: cancellationTokenSource.Token)
+        );
 
-        // actions.LookForObstacles();
-        await actions.FollowLine(LineColour.Red);
-    })
-
-    .OnExit(transition =>
-    {
-        if (transition.Trigger == RobotTrigger.IntersectionDetected)
+        cancellationTokenSource.Cancel();
+        
+        while (stoppedReason == StopReason.TooClose || stoppedReason == StopReason.NoLine)
         {
-            currentConnection = route[intersectionCount];
-            intersectionCount++;
-        }
-    })
-    .OnExitAsync(async () => await robot.SetWheelSpeed(0))
-
-    .Permit(RobotTrigger.NoLineDetected, RobotState.RedStopped)
-    .Permit(RobotTrigger.ObstacleTooClose, RobotState.RedStopped)
-    .Permit(RobotTrigger.Pause, RobotState.RedStopped)
-    .Permit(RobotTrigger.IntersectionDetected, RobotState.NavigatingIntersection);
-
-robotState.Configure(RobotState.NavigatingIntersection)
-    .OnEntry(() => Task.Run(async () =>
-    {
-        Console.WriteLine(currentConnection);
-        Console.WriteLine(intersectionCount);
-
-        if (currentConnection == ConnectionType.CollectBox)
-        {
-            _ = Task.Run(() => robotState.SafeFireAsync(RobotTrigger.CollectBox));
-        }
-        else if (currentConnection == ConnectionType.DropBox)
-        {
-            _ = Task.Run(() => robotState.SafeFireAsync(RobotTrigger.DropBox));
-        }
-        else if (currentConnection == ConnectionType.BlueLine)
-        {
-            _ = Task.Run(() => robotState.SafeFireAsync(RobotTrigger.FollowBlueLine));
-        }
-        else
-        {
-            await robot.Move(currentConnection switch
+            if (stoppedReason == StopReason.TooClose)
             {
-                ConnectionType.Left or ConnectionType.Right => 0.3f,
-                ConnectionType.Forward => 0.2f,
-                _ => 0
-            }, 0, 0);
-
-            await Task.Delay(currentConnection switch
+                await actions.LookForNoObstacles();
+            }
+            else
             {
-                ConnectionType.Left or ConnectionType.Right => 3000,
-                ConnectionType.Forward => 2000,
-                _ => 0
-            });
+                await actions.FindLine(LineColour.Red);
+            }
 
-            await robot.Move(0, 0, currentConnection switch
-            {
-                ConnectionType.Left => -90,
-                ConnectionType.Right => 90,
-                _ => 0
-            });
+            cancellationTokenSource = new CancellationTokenSource();
+        
+            stoppedReason = await await Task.WhenAny
+            (
+                actions.FollowLine(LineColour.Red, cancellationToken: cancellationTokenSource.Token),
+                actions.LookForObstacles(cancellationToken: cancellationTokenSource.Token)
+            );
 
-            await Task.Delay(currentConnection switch
-            {
-                ConnectionType.Left or ConnectionType.Right => 3000,
-                _ => 0
-            });
-
-            _ = Task.Run(() => robotState.SafeFireAsync(RobotTrigger.FinishedNavigatingIntersection));
+            cancellationTokenSource.Cancel();
         }
-    }))
+    }
+    #endregion
 
-    .Permit(RobotTrigger.CollectBox, RobotState.CollectingBox)
-    .Permit(RobotTrigger.DropBox, RobotState.DroppingBox)
-    .Permit(RobotTrigger.FollowBlueLine, RobotState.FollowingBlueLine)
-    .Permit(RobotTrigger.FinishedNavigatingIntersection, RobotState.FollowingRedLine);
+    intersectionCount++;
 
-robotState.Configure(RobotState.FollowingBlueLine)
-    .SubstateOf(RobotState.NavigatingIntersection)
+    if (intersectionCount == route.Count) break;
 
-    .OnEntryAsync(async () =>
+    var currentConnection = route[intersectionCount];
+
+    if (currentConnection == ConnectionType.CollectBox)
     {
-        var actions = new Actions(robot, robotState);
-
-        // actions.LookForObstacles();
-        await actions.FollowLine(LineColour.Blue);
-    })
-
-    .OnExitAsync(async transition =>
-    {
-        if (transition.Trigger == RobotTrigger.IntersectionDetected)
-        {
-            await robot.SetWheelSpeed(60);
-            await Task.Delay(500);
-        }
-
-        await robot.SetWheelSpeed(0);
-    })
-
-    .Permit(RobotTrigger.ObstacleTooClose, RobotState.BlueStopped)
-    .Permit(RobotTrigger.Pause, RobotState.BlueStopped)
-    .Permit(RobotTrigger.NoLineDetected, RobotState.FollowingRedLine)
-    .Permit(RobotTrigger.IntersectionDetected, RobotState.FollowingRedLine);
-
-robotState.Configure(RobotState.CollectingBox)
-    .InitialTransition(RobotState.MovingToBox);
-
-robotState.Configure(RobotState.MovingToBox)
-    .SubstateOf(RobotState.CollectingBox)
-
-    .OnEntry(() => Task.Run(() => Utils.MoveToDepot(robot, robotState)))
-
-    .OnExitAsync(async () => await robot.SetWheelSpeed(0))
-
-    .Permit(RobotTrigger.ObstacleTooClose, RobotState.GrabbingBox)
-    .Permit(RobotTrigger.NoLineDetected, RobotState.GrabbingBox)
-    .Permit(RobotTrigger.IntersectionDetected, RobotState.GrabbingBox);
-
-robotState.Configure(RobotState.GrabbingBox)
-    .SubstateOf(RobotState.CollectingBox)
-
-    .OnEntry(() => Task.Run(async () =>
-    {
+        await actions.MoveToDepot();
+        
         await robot.SetArmPosition(180, 100);
-        await Task.Delay(2000);
+        await Task.Delay(1000);
 
         await robot.CloseGripper();
         await Task.Delay(2000);
 
         await robot.SetArmPosition(73, 159);
-        await Task.Delay(2000);
+        await Task.Delay(1000);
 
-        _ = Task.Run(() => robotState.SafeFireAsync(RobotTrigger.GrabbedBox));
-    }))
-
-    .Permit(RobotTrigger.GrabbedBox, RobotState.ReturningWithBox);
-
-robotState.Configure(RobotState.ReturningWithBox)
-    .SubstateOf(RobotState.CollectingBox)
-
-    .OnEntry(() => Task.Run(() => Utils.ReturnFromDepot(robot, robotState)))
-
-    .Permit(RobotTrigger.ReturnedFromDepot, RobotState.FollowingRedLine);
-
-robotState.Configure(RobotState.DroppingBox)
-    .InitialTransition(RobotState.MovingToDropPoint);
-
-robotState.Configure(RobotState.MovingToDropPoint)
-    .SubstateOf(RobotState.DroppingBox)
-
-    .OnEntry(() => Task.Run(() => Utils.MoveToDepot(robot, robotState)))
-
-    .OnExitAsync(async () => await robot.SetWheelSpeed(0))
-
-    .Permit(RobotTrigger.ObstacleTooClose, RobotState.PlacingBox)
-    .Permit(RobotTrigger.NoLineDetected, RobotState.PlacingBox)
-    .Permit(RobotTrigger.IntersectionDetected, RobotState.PlacingBox);
-
-robotState.Configure(RobotState.PlacingBox)
-    .SubstateOf(RobotState.DroppingBox)
-
-    .OnEntry(() => Task.Run(async () =>
+        await actions.ReturnFromDepot();
+    }
+    else if (currentConnection == ConnectionType.DropBox)
     {
+        await actions.MoveToDepot();
+        
         await robot.SetArmPosition(180, 100);
-        await Task.Delay(2000);
+        await Task.Delay(1000);
 
         await robot.OpenGripper();
         await Task.Delay(2000);
 
         await robot.SetArmPosition(73, 159);
-        await Task.Delay(2000);
+        await Task.Delay(1000);
 
-        _ = Task.Run(() => robotState.SafeFireAsync(RobotTrigger.DroppedBox));
-    }))
-
-    .Permit(RobotTrigger.DroppedBox, RobotState.ReturningDropPoint);
-
-robotState.Configure(RobotState.ReturningDropPoint)
-    .SubstateOf(RobotState.DroppingBox)
-
-    .OnEntry(() => Task.Run(() => Utils.ReturnFromDepot(robot, robotState)))
-
-    .Permit(RobotTrigger.ReturnedFromDepot, RobotState.FollowingRedLine);
-
-var stoppedReasons = new HashSet<RobotTrigger>() { RobotTrigger.Pause };
-robotState.Configure(RobotState.Stopped)
-    .OnExit(stoppedReasons.Clear)
-
-    .OnEntryFrom(RobotTrigger.NoLineDetected, () => new Actions(robot, robotState).LookForLine())
-    .OnEntryFrom(RobotTrigger.ObstacleTooClose, () => new Actions(robot, robotState).LookForNoObstacles())
-
-    .OnEntryFromAndInternal(RobotTrigger.NoLineDetected, () => stoppedReasons.Add(RobotTrigger.NoLineDetected))
-    .OnEntryFromAndInternal(RobotTrigger.ObstacleTooClose, () => stoppedReasons.Add(RobotTrigger.ObstacleTooClose))
-    .OnEntryFromAndInternal(RobotTrigger.Pause, () => stoppedReasons.Add(RobotTrigger.Pause))
-
-    .InternalTransitionIf(RobotTrigger.LineDetected, _ => stoppedReasons.Count >= 2, () => stoppedReasons.Remove(RobotTrigger.NoLineDetected))
-    .InternalTransitionIf(RobotTrigger.NoObstacles, _ => stoppedReasons.Count >= 2, () => stoppedReasons.Remove(RobotTrigger.ObstacleTooClose))
-    .InternalTransitionIf(RobotTrigger.Resume, _ => stoppedReasons.Count >= 2, () => stoppedReasons.Remove(RobotTrigger.Pause));
-
-robotState.Configure(RobotState.RedStopped)
-    .SubstateOf(RobotState.Stopped)
-
-    .PermitIf(RobotTrigger.LineDetected, RobotState.FollowingRedLine, () => stoppedReasons.Count == 1 && stoppedReasons.Contains(RobotTrigger.NoLineDetected))
-    .PermitIf(RobotTrigger.NoObstacles, RobotState.FollowingRedLine, () => stoppedReasons.Count == 1 && stoppedReasons.Contains(RobotTrigger.ObstacleTooClose))
-    .PermitIf(RobotTrigger.Resume, RobotState.FollowingRedLine, () => stoppedReasons.Count == 1 && stoppedReasons.Contains(RobotTrigger.Pause));
-
-robotState.Configure(RobotState.BlueStopped)
-    .SubstateOf(RobotState.Stopped)
-
-    .PermitIf(RobotTrigger.NoObstacles, RobotState.FollowingBlueLine, () => stoppedReasons.Count == 1 && stoppedReasons.Contains(RobotTrigger.ObstacleTooClose))
-    .PermitIf(RobotTrigger.Resume, RobotState.FollowingBlueLine, () => stoppedReasons.Count == 1 && stoppedReasons.Contains(RobotTrigger.Pause));
-#endregion
-
-Console.Write("Enter direct control mode? (y/n, default n): ");
-
-if (Console.ReadKey().KeyChar == 'y')
-{
-    Console.WriteLine();
-    Console.WriteLine("Direct control mode enabled");
-
-
-    Console.Write("Enter command: ");
-    var command = (Console.ReadLine() ?? "").Split(' ');
-
-    while (command.Length != 0)
+        await actions.ReturnFromDepot();
+    }
+    else if (currentConnection == ConnectionType.BlueLine)
     {
-        await robot.Do(command.Select(arg => new CommandArg(arg)).ToArray());
+        var cancellationTokenSource = new CancellationTokenSource();
 
-        Console.Write("Enter command: ");
-        command = (Console.ReadLine() ?? "").Split(' ');
+        var stoppedReason = await await Task.WhenAny
+        (
+            actions.FollowLine(LineColour.Red, cancellationToken: cancellationTokenSource.Token),
+            actions.LookForObstacles(cancellationToken: cancellationTokenSource.Token)
+        );
+
+        cancellationTokenSource.Cancel();
+
+        while (stoppedReason == StopReason.TooClose || stoppedReason == StopReason.NoLine)
+        {
+            if (stoppedReason == StopReason.TooClose)
+            {
+                await actions.LookForNoObstacles();
+            }
+            else
+            {
+                await actions.FindLine(LineColour.Red);
+            }
+
+            cancellationTokenSource = new CancellationTokenSource();
+
+            stoppedReason = await await Task.WhenAny
+            (
+                actions.FollowLine(LineColour.Red, cancellationToken: cancellationTokenSource.Token),
+                actions.LookForObstacles(cancellationToken: cancellationTokenSource.Token)
+            );
+
+            cancellationTokenSource.Cancel();
+        }
+    }
+    else
+    {
+        await robot.Move(currentConnection switch
+        {
+            ConnectionType.Left or ConnectionType.Right => 0.3f,
+            ConnectionType.Forward => 0.2f,
+            _ => 0
+        }, 0, 0);
+
+        await Task.Delay(currentConnection switch
+        {
+            ConnectionType.Left or ConnectionType.Right => 3000,
+            ConnectionType.Forward => 2000,
+            _ => 0
+        });
+
+        await robot.Move(0, 0, currentConnection switch
+        {
+            ConnectionType.Left => -90,
+            ConnectionType.Right => 90,
+            _ => 0
+        });
+
+        await Task.Delay(currentConnection switch
+        {
+            ConnectionType.Left or ConnectionType.Right => 3000,
+            _ => 0
+        });
     }
 }
-else
-{
-    await robot.OpenGripper();
-
-    Console.WriteLine("Paused. Press any key to start...");
-    await Task.WhenAny(Task.Run(Console.ReadKey));
-
-    await robot.SetLineRecognitionEnabled();
-    await robot.SetIrEnabled();
-    await robotState.FireAsync(RobotTrigger.Resume);
-
-    Console.WriteLine("Press any key to stop...");
-    await Task.WhenAny(Task.Run(Console.ReadKey));
-
-    await robotState.SafeFireAsync(RobotTrigger.Pause);
-}
-
-robot.Dispose();
